2910264d9efb4f65c84631882b3b2dd096107a2a
René Heunen <reneheunen@gmail.com>
2017-09-06 12:29:37 +0200

    Add initial files to project

---
+++ submarinehunt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  submarinehunt
+//
+//  Created by René Heunen on 2017-09-06.
+//  Copyright © 2017 René Heunen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
